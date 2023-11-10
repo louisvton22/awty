3d0a204b295fca53c912c32e49b127d121b19f93
Louis Ton <lton2@uw.edu>
2023-11-09 20:22:35 -0800

    Finished. Need to test with physical device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "edu.ischool.lton2.awty"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.ischool.lton2.awty"
--- app/src/main/java/edu/ischool/lton2/awty/MainActivity.kt
+++ app/src/main/java/edu/ischool/lton2/awty/MainActivity.kt
@@ -1,60 +1,105 @@
 package edu.ischool.lton2.awty
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
+const val ALARM_ACTIOM = "edu.ischool.lton2.ALARM"
 class MainActivity : AppCompatActivity() {
     var receiver: BroadcastReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val inputText = findViewById<EditText>(R.id.inputText)
+        val activity = this
+        val inputPhone = findViewById<EditText>(R.id.inputPhone)
+        val inputDuration = findViewById<EditText>(R.id.inputDuration)
         val button = findViewById<Button>(R.id.btnStart)
-        button.text = R.string.start.toString()
-        inputText.addTextChangedListener( object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        button.text = this.getString(R.string.start)
+        button.isEnabled = true
+        button.setOnClickListener {
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-
-            override fun afterTextChanged(s: Editable?) {
-                inputText.removeTextChangedListener(this)
-                if (validate(inputText.toString())) {
-                    button.text = R.string.stop.toString()
+            if (validate(inputPhone.text.toString(), inputDuration.text.toString())) {
+                Log.i("MainActivity", "Setting start to stop")
+                handleClick(inputPhone.text.toString(), inputDuration.text.toString(), button)
+                if (button.text == "Start") {
+                    button.text = this.getString(R.string.stop)
                 } else {
-
+                    button.text = this.getString(R.string.start)
                 }
-                inputText.addTextChangedListener(this)
+
+            } else {
+                Toast.makeText(activity, "Invalid parameters", Toast.LENGTH_SHORT).show()
+                inputPhone.setText("")
+                inputDuration.setText("")
+                return@setOnClickListener
             }
 
-        })
-
+        }
     }
 
     // Check whether the phone number entered is valid
-    fun validate(input: String): Boolean {
-        return false
+    fun validate(input: String, duration:String): Boolean {
+        val phonePattern = "^\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}\$"
+        Log.i("MainActivity", " ${Regex(phonePattern).matches(input)}  $input" )
+        Log.i("MainActivity", " ${Regex("[1-9]+[0-9]*").matches(duration)} $duration" )
+        return Regex(phonePattern).matches(input) && Regex("[1-9]+[0-9]*").matches(duration)
     }
 
-    fun handleClick() {
-        val activityThis = this
-        if (receiver == null) {
-            receiver = object: BroadcastReceiver() {
-                override fun onReceive(context: Context?, intent: Intent?) {
-                    Toast.makeText(activityThis, "Hello", Toast.LENGTH_SHORT).show()
+    fun handleClick(phone: String, duration: String, button: Button) {
+        val duration = duration.toInt()
+        if (button.text == "Start") {
+            val activityThis = this
+            if (receiver == null) {
+                receiver = object : BroadcastReceiver() {
+                    override fun onReceive(context: Context?, intent: Intent?) {
+                        Toast.makeText(
+                            activityThis,
+                            "${formatPhone(phone)}: Are we there yet?",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        Log.i("MainActivity", "Pushed toast message")
+                    }
+
                 }
+                val filter = IntentFilter(ALARM_ACTIOM)
+                registerReceiver(receiver, filter)
+            }
+            // make pending intent
+            val intent = Intent(ALARM_ACTIOM)
+            val pendingIntent =
+                PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            Log.i("MainActivity", "$duration")
+            val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmManager.setRepeating(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + duration * 60000L,
+                duration * 60000L,
+                pendingIntent
+            )
+        } else {
+            Log.i("MainActivity", "Removing receiver")
+            receiver?.let {
+                unregisterReceiver(it)
+                receiver = null
             }
         }
-        val filter = IntentFilter()
-        registerReceiver(receiver, filter)
+    }
+
+    fun formatPhone(phone: String): String {
+        return "(${phone.slice(1..3)}) ${phone.slice(4..6)}-${phone.slice(7..9)} "
     }
 }
